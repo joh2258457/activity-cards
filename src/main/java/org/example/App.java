a7c49d858e90bed4c11a6e89d8554ca7ede5ddf1
joh2258457 <joh2258457@maricopa.edu>
2023-09-12 19:29:00 -0700

    just updated comment

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ * Hi world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
